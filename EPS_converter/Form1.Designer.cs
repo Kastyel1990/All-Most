2027a900a1479f403e6ba39b76d5c1ca593c33a4
Kastyel1990 <Kastyel@ukr.net>
2022-09-21 11:07:04 +0300

    EPS

---
+++ EPS_converter/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace PicToCSV_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightYellow;
+            this.textBox1.Location = new System.Drawing.Point(0, 1);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(312, 25);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Исходный путь";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(318, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.LightYellow;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(0, 94);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(433, 377);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "insert.png");
+            this.imageList1.Images.SetKeyName(1, "folder_black1.png");
+            this.imageList1.Images.SetKeyName(2, "up.png");
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(318, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Конвертировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightYellow;
+            this.textBox2.Location = new System.Drawing.Point(0, 32);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(312, 25);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Конечный путь";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(318, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 25);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Открыть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.progressBar1.Location = new System.Drawing.Point(0, 477);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(433, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "JPG",
+            "PNG",
+            "BMP",
+            "CSV"});
+            this.comboBox1.Location = new System.Drawing.Point(0, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(312, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "Формат конечного файла";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 500);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EPS_converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ EPS_converter/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using ZXing;
+using ImageMagick;
+
+namespace PicToCSV_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string filename;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderIn = new FolderBrowserDialog();
+            folderIn.ShowNewFolderButton = false;
+            DialogResult resultIn = folderIn.ShowDialog();
+            if (resultIn == DialogResult.OK)
+            {
+                textBox1.Text = folderIn.SelectedPath;
+                get_local_filelist(textBox1.Text);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderOut = new FolderBrowserDialog();
+            folderOut.ShowNewFolderButton = true;
+            DialogResult resultout = folderOut.ShowDialog();
+            if (resultout == DialogResult.OK)
+                textBox2.Text = folderOut.SelectedPath;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (new[] { "JPG", "PNG", "BMP", "CSV" }.Any(c => comboBox1.Text.Contains(c)) != true)
+            {
+                MessageBox.Show("Я не знаю во что конвертировать", "Не указан формат конечного файла!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (textBox1.Text.Contains(@":\") != true)
+            {
+                MessageBox.Show("Я не знаю откуда брать файлы", "Не указан исходный путь!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (textBox2.Text.Contains(@":\") != true)
+            {
+                MessageBox.Show("Я не знаю куда ложить файлы", "Не указан конечный путь!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (comboBox1.Text == "CSV")
+            {
+                var temp_path = $@"{Application.StartupPath}\tmp";
+                eps_to_pic(textBox1.Text, temp_path);
+                pic_to_csv(temp_path, textBox2.Text);
+
+                foreach (FileInfo file in new DirectoryInfo(temp_path).GetFiles())
+                {
+                    file.Delete();
+                }
+            }
+            else
+            {
+                eps_to_pic(textBox1.Text, textBox2.Text);
+            }
+            
+            
+            MessageBox.Show("Конвертация завершена.", "Успех!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void eps_to_pic(string in_path, string out_path)
+        {
+            var files = Directory.GetFiles(in_path + @"\", "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".eps"));
+            MagickNET.SetGhostscriptDirectory($@"{Application.StartupPath}");
+            var settings = new MagickReadSettings();
+            settings.Width = 600;
+            settings.Height = 600;
+            settings.Format = MagickFormat.Eps;
+            settings.Density = new Density(600, 600);
+
+
+            var filecount = files.Count();
+            progressBar1.Value = 0;
+            progressBar1.Maximum = filecount;
+
+
+            try
+            {
+                foreach (var file in files)
+                {
+                    //MessageBox.Show(file);
+                    string filename = file.Substring(file.LastIndexOf(@"\") + 1).Substring(0, file.Substring(file.LastIndexOf(@"\") + 1).LastIndexOf(@"."));
+
+                    using (var image = new MagickImage(file, settings))
+                    {
+                        switch (comboBox1.Text)
+                        {
+                            case "JPG":
+                                image.Format = MagickFormat.Jpg;
+                                break;
+                            case "PNG":
+                                image.Format = MagickFormat.Png;
+                                break;
+                            case "BMP":
+                                image.Format = MagickFormat.Bmp;
+                                break;
+                            case "CSV":
+                                image.Format = MagickFormat.Jpg;
+                                break;
+                        }
+                        if (comboBox1.Text == "CSV")
+                            image.Write($@"{out_path}\{filename}.jpg");
+                        else
+                            image.Write($@"{out_path}\{filename}.{comboBox1.Text.ToLower()}");
+                    }
+
+                    progressBar1.Value += 1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($@"{ex.ToString()}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void get_local_filelist(string path)
+        {
+            listView1.Clear();
+            listView1.Items.Add("...", 2);
+            path = path + @"\";
+            foreach (var i in Directory.GetDirectories(path))
+                listView1.Items.Add(i.Substring(i.LastIndexOf(@"\") + 1), 1);
+            foreach (var i in Directory.GetFiles(path))
+                listView1.Items.Add(i.Substring(i.LastIndexOf(@"\") + 1), 0);
+        }
+
+        private void pic_to_csv(string in_path, string out_path)
+        {
+            filename = in_path.Substring(in_path.LastIndexOf(@"\") + 1, in_path.Length - (in_path.LastIndexOf(@"\") + 1));
+            StreamWriter sw = new StreamWriter($@"{out_path}\{filename}.csv");
+            var files = Directory.GetFiles(in_path + @"\", "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".jpg")
+                                                                                                             || s.EndsWith(".png")
+                                                                                                             || s.EndsWith(".bmp")
+                                                                                                             || s.EndsWith(".gif"));
+            var filecount = files.Count();
+            progressBar1.Value = 0;
+            progressBar1.Maximum = filecount;
+
+            try
+            {
+                foreach (var file in files)
+                {
+                    BarcodeReader reader = new BarcodeReader
+                    {
+                        AutoRotate = true,
+
+                        Options =
+                        {
+                        PossibleFormats = new List<BarcodeFormat>{BarcodeFormat.DATA_MATRIX},
+                        TryHarder = true,
+                        ReturnCodabarStartEnd = true,
+                        PureBarcode = false
+                        }
+                    };
+                    Bitmap bitmap = (Bitmap)Image.FromFile(file);
+                    var barcode = reader.Decode(bitmap);
+                    bitmap.Dispose();
+                    sw.WriteLine($"{barcode.ToString()}");
+                    progressBar1.Value += 1;
+
+                };
+                sw.Close();
+                get_local_filelist(in_path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($@"{ex.ToString()}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+    }
+}
